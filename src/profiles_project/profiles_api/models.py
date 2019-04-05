b2164e2c68ccaa7f9fe96df999c8a5c2024b0299
Jacobo Tapia <jacob.et.cetera@gmail.com>
2019-04-04 18:05:00 -0600

    Ok

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -11,21 +11,19 @@ class UserProfileManager(BaseUserManager):
         """ Creates a new user profile object """
         if not email:
             raise ValueError("User must have an email address");
-
         email = self.normalize_email(email)
-
-        user = self.model(email = email, name = name)
-
+        user = self.model(email=email, name=name)
         user.set_password(password)
-
+        user.save(using=self._db)
         return user
 
-    def crate_super_user(self, email, name, password):
-        """ Creates a new super user """
-        user = self.create_user(name, email, password)
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details."""
+        user = self.create_user(email, name, password)
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
+        return user
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """ Represents a "user profile" inside our system """
